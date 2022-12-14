0b0e895fc13d3ef36489681f7dbb49efadec14aa
“Pablo <pmilitelloaveni@gmail.com>
2022-12-14 13:01:52 -0300

    agrego archivos JS de acuerdo a rúbrica

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./styles/App.scss";
+import "./styles/navbar.scss";
+import "./components/NavBar.js";
+import "./components/ItemListContainer";
 
 function App() {
   return (
@@ -9,12 +12,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
