6fe6d5e898be28b992d2a8332b984a980341ea08
“Pablo <pmilitelloaveni@gmail.com>
2022-12-16 19:41:47 -0300

    agrego logo

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
-import logo from "./logo.svg";
 import "./styles/App.scss";
 import "./styles/navbar.scss";
+import "./styles/base/reset.scss";
+import "./styles/base/typography.scss";
 import "./components/NavBar.js";
 import "./components/ItemListContainer";
+import NavBar from "./components/NavBar.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+      <NavBar />
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,1 +1,37 @@
 import "./CartWidget.js";
+
+function NavBar() {
+  return (
+    <header className="header">
+      <div className="div__logo">
+        <img className="logoBeSelf" src="/images/beSelfLogo.svg" alt="logoBeself" />
+      </div>
+      <nav className="navBar">
+        <ul className="unList">
+          <li>
+            <a href="/" className="linkNav">
+              Inicio
+            </a>
+          </li>
+          <li>
+            <a href="/" className="linkNav">
+              Nosotros
+            </a>
+          </li>
+          <li>
+            <a href="/" className="linkNav">
+              Tratamientos
+            </a>
+          </li>
+          <li>
+            <a href="/" className="linkNav">
+              Contacto
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default NavBar;
