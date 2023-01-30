43ed1736f2991efa9c08ecd57dd65b5af39cbb43
“Pablo <pmilitelloaveni@gmail.com>
2023-01-30 10:48:06 -0300

    se embelleció el cartContainer y centró el Loader

--- src/components/CartContainer/CartContainer.js
+++ src/components/CartContainer/CartContainer.js
@@ -9,45 +9,58 @@ function CartContainer() {
 
   if (cart.length === 0) {
     return (
-      <div>
-        <p>
+      <div className="cartClean">
+        <p className="cartTextClean">
           <b>Su carrito está vacio</b>
         </p>
+        <img src="../images/cartEmpty.png" alt="imgCartEmpty" />
       </div>
     );
   } else {
     return (
-      <div className="cartContainer">
-        <div className="divCartButton">
-          <b>Mi compra (3 productos)</b>
-        </div>
-        <div className="divCartItems">
-          <div>
+      <div className="cartListContainer">
+        <b>Detalle de tu compra</b>
+        <table className="cartList">
+          <thead className="cartList_head">
+            <tr className="cartList_row">
+              <th>Miniatura</th>
+              <th>Titulo</th>
+              <th>Precio</th>
+              <th>Cantidad</th>
+              <th>Subtotal</th>
+              <th>Remover</th>
+            </tr>
+          </thead>
+          <tbody>
             {cart.map((itemInCart) => (
-              <div key={itemInCart.id} className="divCartSubItems">
-                <span>
-                  <img src={itemInCart.url} height="50" alt="imgPreviewCart" />
-                </span>
-                <h5>{`${itemInCart.count}  Un.`}</h5>
-                <h5>{`${itemInCart.title}`}</h5>
-                <h5>{`$${itemInCart.price}`}</h5>
-                <h5>Subtotal {itemInCart.price * itemInCart.count}</h5>
-                <Button padding="2px 6px" text="X" color="coral" onClick={() => removeItem(itemInCart)} />
-              </div>
+              <tr key={itemInCart.id} className="cartList_row">
+                <td>
+                  <img height={50} src={itemInCart.url} alt={itemInCart.title} />
+                </td>
+                <td>{itemInCart.title}</td>
+                <td>$ {itemInCart.price}</td>
+                <td>{itemInCart.count}</td>
+                <th>{itemInCart.price * itemInCart.count}</th>
+                <td>
+                  <Button padding="2px 6px" text="X" color="coral" onClick={() => removeItem(itemInCart)} />
+                </td>
+              </tr>
             ))}
-          </div>
+          </tbody>
+        </table>
+
+        <div className="cartList_detail">
+          <h4>El total de tu compra es de ${getTotalPriceInCart()}</h4>
         </div>
-        <div className="divCartTotal">
-          <h4>El total de tu compra es de $ {getTotalPriceInCart()}</h4>
-          <div className="cartButtons">
-            <Link to={`/checkout`}>
-              <Button padding="5px 10px" text="Finalizar compra" color="green" />
-            </Link>
-            <Button onClick={clearCart} padding="5px 10px" color="coral" text="Vaciar carrito"></Button>
-            <Link to="/">
-              <Button padding="5px 10px" color="#F0C808" text="Seguir comprando"></Button>
-            </Link>
-          </div>
+
+        <div className="cartButtons">
+          <Link to={`/checkout`}>
+            <Button padding="5px 10px" text="Finalizar compra" color="green" />
+          </Link>
+          <Button onClick={clearCart} padding="5px 10px" color="coral" text="Vaciar carrito"></Button>
+          <Link to="/">
+            <Button padding="5px 10px" color="#F0C808" text="Seguir comprando"></Button>
+          </Link>
         </div>
       </div>
     );
